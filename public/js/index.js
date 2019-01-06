f0005a01811432bb4af0c494a4d72448f0ae0409
Saravana Balaji <saravana.srinivasan@sapient.com>
2019-01-06 20:07:36 +0530

    added form to chat app

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,6 @@
+
+//import $ from 'jquery';
+
 var socket = io();
 
         socket.on('connect', () => {
@@ -8,7 +11,32 @@ var socket = io();
             console.log('disconnected from server')
         })
         
+        socket.on('newMessage', (message) =>{
+            console.log('newMessage', message);
+            var li = $('<li></li>');
+            li.text(`${message.from} : ${message.text}`)
+
+            $('#message').append(li);
+        })
 
         socket.on('newMessage', (message) =>{
             console.log('newMessage', message)
+        })
+
+        // socket.emit('createMessage', {
+        //     from: 'Esha',
+        //     text: 'Oye'
+        // }, function(data){
+        //     console.log('Got it', data)
+        // })
+
+        $('#message-form').on('submit', function (e) {
+            e.preventDefault();
+
+            socket.emit('createMessage', {
+                from: 'User',
+                text: $('[name=message]').val()
+            }, function(data){
+                console.log('Got it',data)
+            })
         })
